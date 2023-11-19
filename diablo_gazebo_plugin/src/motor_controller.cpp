fb3e1cc39bfa3bafbb7aaf06851c1cd6b693ff34
jya9538 <jya9538@nyu.edu>
2023-11-19 18:21:23 +0400

    initial commit

---
+++ diablo_gazebo_plugin/src/motor_controller.cpp
@@ -0,0 +1,169 @@
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <std_msgs/msg/float64.h>
+
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <sstream>
+#include <Eigen/Dense>
+
+#include "rclcpp/rclcpp.hpp"
+#include "gazebo_ros/node.hpp"
+
+
+namespace gazebo
+{
+class GenericMotorPlugin : public ModelPlugin
+{
+// public: ros::NodeHandle nh;
+// public: ros::Publisher torque_feedback;
+// public: ros::Subscriber position_cmd, velocity_cmd;
+// private: int mode;
+// private: double cmd, integral, Kp, Kd, Ki;
+// private: physics::JointPtr motor_joint;
+// private: physics::ModelPtr model;
+// private: event::ConnectionPtr updateConnection;
+
+private:
+    int mode;
+    double cmd, integral, Kp, Kd, Ki;
+    physics::JointPtr motor_joint;
+    physics::ModelPtr model;
+    event::ConnectionPtr updateConnection;
+
+public:
+    rclcpp::Node::SharedPtr nh;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr torque_feedback;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr position_cmd, velocity_cmd;
+    double LIM_;
+
+    void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+	{
+	  	this->model = _parent;			
+		this->mode = 0; //default mode is position
+		this->cmd = 0.0;
+		this->integral = 0.0;
+		this->Kp = 0.5;
+		this->Kd = 0.05;
+		this->Ki = 0.1;
+		LIM_ = 0.5;
+
+		std::string name;
+		std::string ns;
+		if(_sdf->HasElement("namespace"))
+			ns = _sdf->GetElement("namespace")->Get<std::string>();							
+		if(_sdf->HasElement("joint_name"))
+			name = _sdf->GetElement("joint_name")->Get<std::string>();		
+		if(_sdf->HasElement("Kp"))
+			this->Kp = _sdf->GetElement("Kp")->Get<double>();				
+		if(_sdf->HasElement("Kd"))
+			this->Kd = _sdf->GetElement("Kd")->Get<double>();				
+		if(_sdf->HasElement("Ki"))
+			this->Ki = _sdf->GetElement("Ki")->Get<double>();				
+			
+		this->motor_joint = this->model->GetJoint(ns+"::"+name);		
+		
+		std::string pos_topic = ns + "/" + name + "/pos_cmd" ;
+		std::string vel_topic = ns + "/" + name + "/vel_cmd" ;
+
+		// position_cmd = nh.subscribe(pos_topic, 1, &GenericMotorPlugin::pos_cmd_callback, this); 
+        position_cmd = nh->create_subscription<std_msgs::msg::Float64>(pos_topic, 10, std::bind(&GenericMotorPlugin::pos_cmd_callback, this));
+		// velocity_cmd = nh.subscribe(vel_topic, 1, &GenericMotorPlugin::vel_cmd_callback, this); 	
+        velocity_cmd = nh->create_subscription<std_msgs::msg::Float64>(vel_topic, 10, std::bind(&GenericMotorPlugin::vel_cmd_callback, this));
+		// torque_feedback = nh.advertise<std_msgs::Float64>(ns+"/"+name+"/applied_torque", 1);
+        torque_feedback = nh->create_publisher<std_msgs::msg::Float64>(ns+"/"+name+"/applied_torque");
+		
+	  	this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&GenericMotorPlugin::onUpdate, this));
+		
+	}  
+    
+    void onUpdate()
+	{
+		double torque = 0.0;
+		double angle = this->motor_joint->Position(0);
+		double rate = this->motor_joint->GetVelocity(0);
+
+		if(mode==0){
+			this->integral += 0.005 * (angle - this->cmd) ; 
+			if(this->integral > LIM_){
+				this->integral = LIM_;
+			}
+			if(this->integral < -LIM_){
+				this->integral = -LIM_;
+			}
+			torque = -(this->Kp)*(angle - this->cmd) - 2.0*rate - 0.3*this->integral;			
+			this->motor_joint->SetForce(0,torque);
+			std_msgs::msg::Float64 feedback_msg;
+			feedback_msg.data = torque;
+			torque_feedback->publish(feedback_msg);
+			return;
+		}
+		if(mode==1){
+			this->integral += 0.005 * (rate - this->cmd) ; 
+			if(this->integral > LIM_){
+				this->integral = LIM_;
+			}
+			if(this->integral < -LIM_){
+				this->integral = -LIM_;
+			}
+			torque = -(this->Kd)*(rate - this->cmd) - (this->Ki)*this->integral;
+			this->motor_joint->SetForce(0,torque);
+			std_msgs::msg::Float64 feedback_msg;
+			feedback_msg.data = torque;
+			torque_feedback->publish(feedback_msg);
+			return;
+		}
+
+	}
+
+    void pos_cmd_callback(const std_msgs::msg::Float64& msg)
+	{
+		if(this->mode != 0){
+			this->integral = 0.0;
+		}
+		this->mode = 0;
+		this->cmd = msg.data;
+		return;		
+	}
+
+    void vel_cmd_callback(const std_msgs::msg::Float64& msg)
+	{
+		if(this->mode != 1){
+			this->integral = 0.0;
+		}
+		this->mode = 1;
+		this->cmd = msg.data;
+		return;		
+	}
+
+
+
+};
+GZ_REGISTER_MODEL_PLUGIN(GenericMotorPlugin)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ diablo_simulation/launch/gazebo2.launch.py
@@ -0,0 +1,43 @@
+import os
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+def generate_launch_description():
+    share_dir = FindPackageShare(package='diablo_simulation').find('diablo_simulation')
+
+    world_file_name = 'diablo.world'
+    world_path = os.path.join(share_dir, 'worlds', world_file_name)
+
+    gazebo_models_path = os.path.join(share_dir, 'models')
+    os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'true',
+            'world': world_path
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+    return LaunchDescription([
+        gazebo_server,
+        gazebo_client
+    ])
